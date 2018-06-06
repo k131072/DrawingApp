cb36ad72c9ae782885b1e04774cf707c8c663a3b
KeY <nicekey@hotmail.com>
2018-06-06 22:49:14 +0900

    first

---
+++ DrawingApp/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace DrawingApp
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void OpenFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            var picker = new FileOpenPicker();
+            picker.FileTypeFilter.Add(".png");
+            picker.FileTypeFilter.Add(".jpg");
+            picker.FileTypeFilter.Add(".jpeg");
+
+            picker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+
+            var file = await picker.PickSingleFileAsync();
+            if (file == null)
+            {
+                return;
+            }
+
+            using (var stream = await file.OpenAsync(FileAccessMode.Read))
+            {
+                var image = new BitmapImage();
+                await image.SetSourceAsync(stream);
+                picture.Source = image;
+            }
+        }
+    }
+}
